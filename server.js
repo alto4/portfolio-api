b3594fad81cbf3abc831b5c454cb1f578a456261
alto4 <scottaltonmusic@gmail.com>
2022-10-10 18:32:23 -0400

    Add CORs fix.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,12 @@ mongoose.connect(process.env.DATABASE_URL);
 
 // Middleware
 app.use(express.json());
+app.use(
+  cors({
+    // origin: 'http://localhost:3000',
+    origin: 'https://portfolio-2022-lyart-xi.vercel.app/',
+  })
+);
 
 // Routes
 const contactRouter = require('./routes/contact');
